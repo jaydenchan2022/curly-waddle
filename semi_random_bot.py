6a45d6950da8987a78a5ade8917bc80e076756ab
benjaminluo-lgtm <benjaminluo@hunterschools.org>
2025-10-22 22:33:26 -0400

    Update and rename monkeys_bananas_game.py to semi_random_bot.py

--- semi_random_bot.py
+++ semi_random_bot.py
@@ -7,14 +7,14 @@ def bananabid(my_player_number: int, my_bananas: int, monkey_position: int, oppo
     elif monkey_position==2*(my_player_number*2-3):
         return my_bananas
     elif monkey_position==-2*(my_player_number*2-3):
-        return my_bananas-4
+        return min(my_bananas-4,opponent_bananas+1)
     elif my_bananas<=5:
         return 1
     else:
         lastBid=past_bid_list[turn_number-2][2-my_player_number]
         if lastBid>26 and lastBid<58:
-            return min(my_bananas,lastBid+randint(2,12))
+            return min(my_bananas-abs(monkey_position-3*(my_player_number*2-3))+1,lastBid+randint(2,12))
         elif lastBid<=26:
-            return min(my_bananas,2*lastBid+randint(1,6))
+            return min(my_bananas-abs(monkey_position-3*(my_player_number*2-3))+1,2*lastBid+randint(1,6))
         elif lastBid>=58:
-            return min(my_bananas,int(lastBid/2)+randint(2,12))
+            return min(my_bananas-abs(monkey_position-3*(my_player_number*2-3))+1,int(lastBid/2)+randint(2,12))
