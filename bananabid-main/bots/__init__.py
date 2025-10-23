acfab25b25dda5b8b780b0eb0904ac7a43d4b4c3
benjaminluo-lgtm <benjaminluo@hunterschools.org>
2025-10-22 22:43:18 -0400

    Delete bananabid-main directory

--- bananabid-main/bots/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for module in os.listdir(os.path.dirname(__file__)):
-    if module == '__init__.py' or not module.endswith('.py'):
-        continue
-    __import__("bots." + module[:-3], locals(), globals())
